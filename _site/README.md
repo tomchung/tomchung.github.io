ab712a3429b83c0770e0f862abdb84463a300574
tom chung <tommmchung@gmail.com>
2016-03-14 01:02:09 -0400

    photo edits
    working title, spun lights

---
+++ _site/README.md
@@ -0,0 +1,37 @@
+### 1. Sync your project
+Open the github app and sync both the `tomchung.github.io` and `tomchung.net` projects to make sure you have the latest version on you computer.
+
+### 2. Run the website locally
+Open terminal.app and type the following two commands:
+
+`cd ~/Documents/Website/tomchung.github.io-source`
+
+`jekyll serve --skip-initial-build --watch`
+
+Navigate to `localhost:4000` in your web browser. The website is now running locally.
+
+### 3. Create a new project
+Open the `_Posts` folder and duplicate and rename any of the markdown files to your liking. The date prepended to the filename dictates the order in which the projects appear on the website menu, and the name of the file dictates the URL. This new file represents the content of your new project.
+
+Your file has the following fields you need to edit:
+
+`title` - Defines the title of your project.
+
+`year` - Defines the year the project was completed.
+
+`layout` - Keep this as ‘Post’.
+
+`tags` - This used to be typologies but is no longer used on the site.
+
+`images` - Defines the source of the images. Place images in the `public/images` folder.
+
+`size` - Specifies whether an image is displayed full width or two up. Can be defined as `full` or `half`. Half size images must occur two in a row and have the same pixel dimensions to render properly.
+
+Once you’ve saved your file your changes will be live at `localhost:4000` in your web browser. Once you’re happy with your changes you’re ready to publish them.
+
+### 4. Publish your changes
+Once you’re finished making your changes it’s time to publish them. Open the Github app and note that you have changes to both the `tomchung.github.io` and `tomchung.net` projects. Type a ‘summary’ of the changes you’ve made and hit the ‘commit to master’ button.
+
+Lastly hit the ‘sync’ button in the top right, your changes will now be live on http://tomchung.net.
+
+
---
+++ _site/public/js/functions.js
@@ -0,0 +1,119 @@
+function randomize() {
+	items = [
+	    { image : 'umbra-shift/tom-chung-umbra-shift-11.jpg', link : 'umbra-shift' },
+	    { image : 'spun-lights/tom-chung-spun-lights-01.jpg', link : 'spun-lights' },
+	    { image : 'working-title/tom-chung-working-title-10.jpg', link : 'working-title' }
+	]
+	var item = items[Math.floor(Math.random()*items.length)];
+	$('.cover-image').css({ 'background-image' : 'url("public/images/' + item.image + '")'});
+	$('.hero-link').attr({'href' : '/projects/' + item.link });
+}
+
+function reveal() {	
+	window.scrollTo(0, 0);
+	
+	randomize();
+	
+	$('.cover-image').imagesLoaded( { background: true }, function() {
+		$('.cover-image').css({ 'opacity' : 1 });
+	});
+	
+	setTimeout(function() {
+		$('#content').css({ 'opacity' : 1 });
+	}, 50);
+}
+
+function getImageSize(img, callback) {
+    var $img = $(img);
+    var wait = setInterval(function() {
+        var w = $img[0].naturalWidth,
+            h = $img[0].naturalHeight;
+        if (w && h) {
+            clearInterval(wait);
+            callback.apply(this, [w, h]);
+        }
+    }, 30);
+}
+
+function size() {
+	$('.gallery img').each(function(i) {
+		var img = $(this);
+		getImageSize(img, function(width, height) {
+		    var ratio = (height / width) * 100;
+		    img.parent().css({ 'padding-bottom' : ratio + '%' });
+		});	
+	}).promise().done(function() {
+		setTimeout(function() {
+			$('.image-wrap').css({ 'background-color' : '#222' });
+		}, 200);
+/*
+		setTimeout(function() {
+			$('.image-wrap').css({ 'background-color' : '#222' });
+		}, 100);
+		setTimeout(function() {
+			$('.gallery').imagesLoaded().progress(function(instance, image) {
+				$(image.img).css({ 'opacity' : 1 });
+			});
+		}, 500);
+*/
+	});
+}
+
+function hide() {
+	$('#content').css({ 'opacity' : 0 });
+}
+
+function pin() {
+	var stickyColumn   = $('.sticky-column');
+	var contentHeight  = stickyColumn.outerHeight();
+	var windowHeight   = $(window).height();
+	var scrollPosition = $(window).scrollTop();
+	
+	if (contentHeight > windowHeight) {
+		stickyColumn.css({ 'top' : -(contentHeight - windowHeight) });
+	} else {
+		stickyColumn.css({ 'top' : '' });
+	}
+	
+	if (scrollPosition > (contentHeight - windowHeight)) {
+		stickyColumn.addClass('pinned');
+	} else {
+		stickyColumn.removeClass('pinned');
+	}
+}
+
+$(document).ready(function() {
+	reveal();
+	size();
+	
+	document.addEventListener('touchstart', function(){}, true);
+	
+	$(window).bind('load resize scroll', function() {
+		pin();
+	});
+	
+	var siteUrl = 'http://'+(document.location.hostname||document.location.host);
+	
+	$(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click tap", function(e) {
+		e.preventDefault();
+		History.pushState({}, "", this.pathname);
+		hide();
+	});
+
+	History.Adapter.bind(window, 'statechange', function() {
+		var State = History.getState();
+		$.get(State.url, function(data){
+			document.title = data.match(/<title>(.*?)<\/title>/)[1];
+			setTimeout(function() {
+				$('#content').html($(data).find('#content').children());
+				reveal();
+				size();	
+				pin();			
+			}, 500);
+		});
+	});
+	
+	if ('ontouchstart' in document) {
+	    $('body').removeClass('no-touch');
+	}
+});
