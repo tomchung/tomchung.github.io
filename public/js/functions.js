44c715d892bc1459a80476948ecec96f36b81d71
tom chung <tommmchung@gmail.com>
2016-06-04 03:19:09 -0400

    fafa

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,7 +1,7 @@
 function randomize() {
 	items = [
 	    { image : 'umbra-shift/tom-chung-umbra-shift-11.jpg', link : 'umbra-shift' },
-	    { image : 'spun-lights/tom-chung-spun-lights-01.jpg', link : 'spun-lights' },
+	    { image : 'spun-lights/tom-chung-spun-lights-03.jpg', link : 'spun-lights' },
 	    { image : 'working-title/tom-chung-working-title-10.jpg', link : 'working-title' }
 	    { image : '68-claremont/tom-chung-68-claremont-08.jpg', link : '68-claremont' }
 
