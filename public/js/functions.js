a227aa24f2a280b97d7fe213ec8b47d08f631121
Andy Chung <hi@andychung.me>
2016-11-15 21:04:39 -0800

    Staging catalog

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,11 +1,10 @@
 function randomize() {
 	items = [
 	    { image : 'spun-sconce/tom-chung-sc-01.jpg', link : 'spun-sconce' },
-	    { image : 'working-title/tom-chung-working-title-10.jpg', link : 'working-title' }
-	    { image : '68-claremont/tom-chung-68-claremont-07.jpg', link : '68-claremont' }
-	    {image:"laser-cut-stools/tom-chung-laser-cut-stools-01.jpg", link : 'laser-cut-stools}
-
-	]
+	    { image : 'working-title/tom-chung-working-title-10.jpg', link : 'working-title' },
+	    { image : '68-claremont/tom-chung-68-claremont-07.jpg', link : '68-claremont' },
+	    { image : 'laser-cut-stools/tom-chung-laser-cut-stools-01.jpg', link : 'laser-cut-stools' }
+	];
 	var item = items[Math.floor(Math.random()*items.length)];
 	$('.cover-image').css({ 'background-image' : 'url("public/images/' + item.image + '")'});
 	$('.hero-link').attr({'href' : '/projects/' + item.link });
@@ -16,49 +15,17 @@ function reveal() {
 	
 	randomize();
 	
-	$('.cover-image').imagesLoaded( { background: true }, function() {
+	$(".gallery").imagesLoaded().progress(function(t, n) {
+        $(n.img).css({ 'opacity' : 1 })
+    });
+	
+	$('.cover-image').imagesLoaded({ background: true }, function() {
 		$('.cover-image').css({ 'opacity' : 1 });
 	});
 	
 	setTimeout(function() {
 		$('#content').css({ 'opacity' : 1 });
 	}, 50);
-}
-
-function getImageSize(img, callback) {
-    var $img = $(img);
-    var wait = setInterval(function() {
-        var w = $img[0].naturalWidth,
-            h = $img[0].naturalHeight;
-        if (w && h) {
-            clearInterval(wait);
-            callback.apply(this, [w, h]);
-        }
-    }, 30);
-}
-
-function size() {
-	$('.gallery img').each(function(i) {
-		var img = $(this);
-		getImageSize(img, function(width, height) {
-		    var ratio = (height / width) * 100;
-		    img.parent().css({ 'padding-bottom' : ratio + '%' });
-		});	
-	}).promise().done(function() {
-		setTimeout(function() {
-			$('.image-wrap').css({ 'background-color' : '#222' });
-		}, 200);
-/*
-		setTimeout(function() {
-			$('.image-wrap').css({ 'background-color' : '#222' });
-		}, 100);
-		setTimeout(function() {
-			$('.gallery').imagesLoaded().progress(function(instance, image) {
-				$(image.img).css({ 'opacity' : 1 });
-			});
-		}, 500);
-*/
-	});
 }
 
 function hide() {
@@ -69,9 +36,10 @@ function pin() {
 	var stickyColumn   = $('.sticky-column');
 	var contentHeight  = stickyColumn.outerHeight();
 	var windowHeight   = $(window).height();
+	var windowWidth    = $(window).width();
 	var scrollPosition = $(window).scrollTop();
 	
-	if (contentHeight > windowHeight) {
+	if ((contentHeight > windowHeight) && (windowWidth > 1020)) {
 		stickyColumn.css({ 'top' : -(contentHeight - windowHeight) });
 	} else {
 		stickyColumn.css({ 'top' : '' });
@@ -86,7 +54,6 @@ function pin() {
 
 $(document).ready(function() {
 	reveal();
-	size();
 	
 	document.addEventListener('touchstart', function(){}, true);
 	
